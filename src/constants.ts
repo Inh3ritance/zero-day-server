47d97663038dd1923a22857adfe4e0dc70dc318d
Fabian Flores <fabian@fabiandean.dev>
2022-06-04 15:44:44 -0700

    Small file restructure, improve lint & ts

---
+++ src/constants.ts
@@ -0,0 +1,11 @@
+export const SOCKET_EVENTS = {
+  CONNECTION: 'connection',
+  LOGIN: 'login',
+  DISCONNECT: 'disconnect',
+  UPDATE_SOCKET: 'update-socket',
+  PUBLIC_RETRIEVE: 'public-retrieve',
+  PUBLIC_SEND: 'public-send',
+  SEND_REQUEST: 'send-request',
+  SEND_MESSAGE: 'send-message',
+  NEW_MESSAGE: 'new-message',
+} as const;
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
-import { ExtendedError } from "socket.io/dist/namespace";
-import { Socket } from "socket.io/dist/socket";
-import express, { Response, Request } from "express";
+import { ExtendedError } from 'socket.io/dist/namespace';
+import { Socket } from 'socket.io/dist/socket';
+import express, { Response, Request } from 'express';
 import { instrument } from '@socket.io/admin-ui';
 import cryptojs from 'crypto-js';
 import helmet from 'helmet';
@@ -9,6 +9,7 @@ import cors from 'cors';
 import Datastore from 'nedb';
 import { Server } from 'socket.io';
 import { DateTime } from 'luxon';
+import { SOCKET_EVENTS } from './constants';
 
 const app = express();
 const server = http.createServer(app);
@@ -20,12 +21,13 @@ const days = 1000 * 60 * 60 * 24 * 60; // 60 days in milliseconds
 
 const io = new Server(server, {
   cors: {
-    origin: "*",
-  }
+    origin: '*',
+  },
 });
 
 // Use a VPN still!!
-io.use((socket: Socket, next: (err?: ExtendedError) => void) => {
+io.use((socketParam: Socket, next: (err?: ExtendedError) => void) => {
+  const socket = socketParam;
   socket.handshake.address = '';
   next();
 });
@@ -44,22 +46,22 @@ app.get('/', (_: Request, res: Response) => res.send('Server running...'));
 
 app.get('/users', (_: Request, res: Response) => {
   db.find({}, (err: Error | null, docs: never[]) => {
-    if(err) console.log(err);
+    if (err) console.error(err);
     res.send(docs);
   });
 });
 
 app.post('/createUser', (req: Request, res: Response) => { // make this more secure
   db.findOne({ _id: req.body.user }, (err, doc) => {
-    if(err) console.log(err);
-    if(doc === null) {
+    if (err) console.error(err);
+    if (doc === null) {
       db.insert({
         user: req.body.user,
         exp: new Date().getTime() + days,
         pass: sha512(req.body.pass).toString(), // argon
         socket: null,
-      }, (err, _) => {
-        if(err) console.log(err);
+      }, (error, _) => {
+        if (error) console.error(error);
         res.sendStatus(200);
       });
     } else {
@@ -68,18 +70,17 @@ app.post('/createUser', (req: Request, res: Response) => { // make this more sec
   });
 });
 
-io.on('connection', (socket) => {
-
-  socket.on('login', (data) => { // improve
+io.on(SOCKET_EVENTS.CONNECTION, (socket) => {
+  socket.on(SOCKET_EVENTS.LOGIN, (data) => { // improve
     db.findOne({ user: data.user }, (err, doc) => {
-      if(err) console.log(err);
-      if(sha512(data?.pass).toString() === doc?.pass && doc?.socket) { // || cookie, jwt
-        io.to(socket.id).emit('updateSocket', { socket: doc.socket });
+      if (err) console.error(err);
+      if (sha512(data?.pass).toString() === doc?.pass && doc?.socket) { // || cookie, jwt
+        io.to(socket.id).emit(SOCKET_EVENTS.UPDATE_SOCKET, { socket: doc.socket });
         socket.disconnect();
-      } else if(sha512(data?.pass).toString() === doc?.pass) {
-        db.update({ user: data.user }, { $set: { socket: socket.id, exp: new Date().getTime() + days } }, {}, (err, _) => {
-          if(err) console.log(err);
-          io.to(socket.id).emit('updateSocket', { socket: socket.id });
+      } else if (sha512(data?.pass).toString() === doc?.pass) {
+        db.update({ user: data.user }, { $set: { socket: socket.id, exp: new Date().getTime() + days } }, {}, (error, _) => {
+          if (error) console.error(error);
+          io.to(socket.id).emit(SOCKET_EVENTS.UPDATE_SOCKET, { socket: socket.id });
         });
       } else {
         socket.disconnect();
@@ -87,56 +88,58 @@ io.on('connection', (socket) => {
     });
   });
 
-  socket.on('disconnect', () => {
+  socket.on(SOCKET_EVENTS.DISCONNECT, () => {
     db.update({ socket: socket.id }, { $set: { socket: null } }, {}, (err, _) => {
-      if(err) console.log(err);
+      if (err) console.error(err);
     });
   });
 
   // Insecure, in person or other medium preferred, create X3 Diffie Hellman protocol
   // eslint-disable-next-line @typescript-eslint/no-empty-function
-  socket.on('key_exchange', () => {});
+  socket.on('key-exchange', () => {});
 
-  socket.on('sendRequest', (friend) => {
+  socket.on(SOCKET_EVENTS.SEND_REQUEST, (friend) => {
     db.findOne({
-      _id: friend
+      _id: friend,
     }, (err, doc) => {
-      if(err) console.log(err);
-      if(doc !== null && doc?.socket) {
+      if (err) console.log(err);
+      if (doc !== null && doc?.socket) {
         console.log(doc);
-        //io.to(socket).emit('recieveRequest', {  });
+        // io.to(socket).emit('recieveRequest', {  });
       }
     });
   });
 
   // not encrypted, on public chat
-  socket.on('public-send', (data) => {
+  socket.on(SOCKET_EVENTS.PUBLIC_SEND, (dataParam) => {
+    const data = dataParam;
     const datetime = DateTime.utc().toISO();
-    data['datetime'] = datetime;
-    io.sockets.emit('public-retrieve', data);
+    data.datetime = datetime;
+    io.sockets.emit(SOCKET_EVENTS.PUBLIC_RETRIEVE, data);
   });
 
-  socket.on('send message', ({ content, to, sender, chatName, isChannel }) => {
-    if(isChannel) {
+  socket.on(SOCKET_EVENTS.SEND_MESSAGE, ({
+    content, to, sender, chatName, isChannel,
+  }) => {
+    if (isChannel) {
       const payload = {
         content,
         chatName,
-        sender
-      }
-      socket.to(to).emit('new message', payload);
+        sender,
+      };
+      socket.to(to).emit(SOCKET_EVENTS.NEW_MESSAGE, payload);
     } else {
       const payload = {
         content,
         chatName: sender,
-        sender
-      }
-      socket.to(to).emit('new message', payload);
+        sender,
+      };
+      socket.to(to).emit(SOCKET_EVENTS.NEW_MESSAGE, payload);
     }
   });
-
 });
 
 const port = 9000;
 server.listen(port, () => console.log(`Server is running on port ${port}`));
 
-export {}
+export {};
